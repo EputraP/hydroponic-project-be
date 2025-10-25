b59345228fd8e749ed9398324e93ab543a4e23ed
Eka Putra Prasetya <eka.putra@caditglobal.com>
2025-10-26 05:13:06 +0700

    add invalid param

--- internal/errors/errors.go
+++ internal/errors/errors.go
@@ -6,6 +6,7 @@ var (
 	InvalidBearerFormat = errors.New("Invalid Authorization Bearer Format")
 	InvalidToken        = errors.New("Invalid Token")
 	InvalidIssuer       = errors.New("Invalid Token Issuer")
+	InvalidParam        = errors.New("Invalid Parameter")
 
 	ForbiddenAccess = errors.New("user is forbidden to access this resource")
 

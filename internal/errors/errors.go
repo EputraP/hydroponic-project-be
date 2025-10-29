35446de14fd5a75ffdd081292a041e86a2cd5c97
Eka Putra Prasetya <eka.putra@caditglobal.com>
2025-10-30 05:15:29 +0700

    add InvalidUUIDParamFormat

--- internal/errors/errors.go
+++ internal/errors/errors.go
@@ -3,12 +3,15 @@ package errs
 import "errors"
 
 var (
-	InvalidBearerFormat = errors.New("Invalid Authorization Bearer Format")
-	InvalidToken        = errors.New("Invalid Token")
-	InvalidIssuer       = errors.New("Invalid Token Issuer")
-	InvalidParam        = errors.New("Invalid Parameter")
+	InvalidBearerFormat    = errors.New("Invalid Authorization Bearer Format")
+	InvalidToken           = errors.New("Invalid Token")
+	InvalidIssuer          = errors.New("Invalid Token Issuer")
+	InvalidParam           = errors.New("Invalid Parameter")
+	InvalidUUIDParamFormat = errors.New("Invalid UUID Parameter Format")
 
 	ForbiddenAccess = errors.New("user is forbidden to access this resource")
 
 	InvalidRequestBody = errors.New("invalid request body")
+
+	CantFindAnySubModules = errors.New("can't find any sub modules")
 )

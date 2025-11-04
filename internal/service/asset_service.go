acb98ec4d4f4ba10c6727cfd72b5cb7a61010065
Eka Putra Prasetya <eka.putra@caditglobal.com>
2025-11-05 05:51:57 +0700

    add validate id fk

--- internal/service/asset_service.go
+++ internal/service/asset_service.go
@@ -14,15 +14,24 @@ type AssetService interface {
 
 type assetService struct {
 	assetRepo repository.AssetRepository
+	plantRepo repository.PlantRepository
+	uomRepo   repository.UomRepository
+	assetType repository.AssetTypeRepository
 }
 
 type AssetServiceConfig struct {
 	AssetRepo repository.AssetRepository
+	PlantRepo repository.PlantRepository
+	UomRepo   repository.UomRepository
+	AssetType repository.AssetTypeRepository
 }
 
 func NewAssetService(config AssetServiceConfig) AssetService {
 	return &assetService{
 		assetRepo: config.AssetRepo,
+		plantRepo: config.PlantRepo,
+		uomRepo:   config.UomRepo,
+		assetType: config.AssetType,
 	}
 }
 
@@ -30,6 +39,30 @@ func (s *assetService) CreateAsset(input *dto.Asset) (*dto.Asset, error) {
 	logger.Info("assetService", "Creating new asset", map[string]string{
 		"name": input.AssetName,
 	})
+
+	plantById, err := s.plantRepo.GetPlantById(&input.PlantID)
+	if err != nil || len(*plantById) == 0 {
+		logger.Error("assetService", "Invalid PlantID provided", map[string]string{
+			"error": errs.InvalidPlantId.Error(),
+		})
+		return nil, errs.InvalidPlantId
+	}
+
+	uomById, err := s.uomRepo.GetUoMById(&input.UOMID)
+	if err != nil || len(*uomById) == 0 {
+		logger.Error("assetService", "Invalid UOMID provided", map[string]string{
+			"error": errs.InvalidUoMId.Error(),
+		})
+		return nil, errs.InvalidUoMId
+	}
+
+	assetTypeById, err := s.assetType.GetAssetTypeById(&input.AssetTypeID)
+	if err != nil || len(*assetTypeById) == 0 {
+		logger.Error("assetService", "Invalid AssetTypeID provided", map[string]string{
+			"error": errs.InvalidAssetTypeId.Error(),
+		})
+		return nil, errs.InvalidAssetTypeId
+	}
 
 	createdAsset, err := s.assetRepo.CreateAsset(&model.Asset{
 		UOMID:       input.UOMID,

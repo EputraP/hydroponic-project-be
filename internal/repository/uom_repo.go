ab4e4bd8bbe8dcc1d972012b9dee8d69ba00e033
Eka Putra Prasetya <eka.putra@caditglobal.com>
2025-11-05 05:51:12 +0700

    add GetUoMById

--- internal/repository/uom_repo.go
+++ internal/repository/uom_repo.go
@@ -6,11 +6,13 @@ import (
 	"hydroponic-be/internal/util/logger"
 	"strconv"
 
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type UomRepository interface {
 	GetUoMs() (*[]model.Uom, error)
+	GetUoMById(input *uuid.UUID) (*[]model.Uom, error)
 }
 
 type uomRepository struct {
@@ -66,6 +68,57 @@ func (r *uomRepository) GetUoMs() (*[]model.Uom, error) {
 	}
 
 	logger.Info("uomRepository", "GetUoMs fetched successfully", map[string]string{
+		"fetched": strconv.Itoa(len(result)),
+	})
+	return &result, nil
+
+}
+
+func (r *uomRepository) GetUoMById(input *uuid.UUID) (*[]model.Uom, error) {
+
+	logger.Info("uomRepository", "Fetching GetUoMById", map[string]string{})
+
+	queryResult := &model.Uom{}
+	result := []model.Uom{}
+
+	sqlScript := `select
+					id,
+					name,
+					symbol,
+					description
+				from hydroponic_system.uom
+				where
+					deleted_at is null and id = '%s'
+		`
+
+	sqlScript = fmt.Sprintf(sqlScript, input.String())
+
+	rows, err := r.db.Raw(sqlScript).Rows()
+	if err != nil {
+		logger.Error("uomRepository", "Failed to fetch GetUoMById", map[string]string{
+			"error": err.Error(),
+		})
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		if err := rows.Scan(
+			&queryResult.ID,
+			&queryResult.Name,
+			&queryResult.Symbol,
+			&queryResult.Description,
+		); err != nil {
+			logger.Error("uomRepository", "Failed to fetch scan GetUoMById result", map[string]string{
+				"error": err.Error(),
+			})
+			return nil, fmt.Errorf("error scanning row: %w", err)
+		}
+
+		result = append(result, *queryResult)
+	}
+
+	logger.Info("uomRepository", "GetUoMById fetched successfully", map[string]string{
 		"fetched": strconv.Itoa(len(result)),
 	})
 	return &result, nil
